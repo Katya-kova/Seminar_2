f761f00c565140fe2adcdc63404d5ec45df7d706
Katerina <k_by96@mail.ru>
2023-04-03 20:44:02 +0300

    Program added

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+int i = GetRandom();
+
+Console.WriteLine(i);
+Console.WriteLine(ShowSecondDigit(i));
+
+int GetRandom()
+{
+    return new Random().Next(100, 1000);
+}
+
+int ShowSecondDigit(int number)
+{
+    int firstNumber = number % 100;
+    int secondNumber = firstNumber / 10;
+    int result = secondNumber;
+    return result;
+}
